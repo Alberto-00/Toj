7b766769f270e2a31a73cd0797ea413997b23f38
albmo <albmont99@gmail.com>
2021-04-14 21:49:14 +0200

    Servlet Donna

--- Code Toj/src/main/java/Controller/DonnaServlet.java
+++ Code Toj/src/main/java/Controller/DonnaServlet.java
@@ -1,19 +1,31 @@
 package Controller;
 
+import Model.Articolo;
+import Model.ArticoloDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "DonnaServlet", value = "/DonnaServlet")
 public class DonnaServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        ArticoloDAO service = new ArticoloDAO();
+        ArrayList<Articolo> articoli = service.doRetrieveBySex("F");
+        request.setAttribute("articoliDonna", articoli);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/donna.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
     }
 }
