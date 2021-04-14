a1c2d02edb619143ccce459ce86e0bc6aa2c2e50
albmo <albmont99@gmail.com>
2021-04-14 11:35:22 +0200

    Changes by Utente

--- Code Toj/src/main/java/Model/Utente.java
+++ Code Toj/src/main/java/Model/Utente.java
@@ -1,4 +1,112 @@
 package Model;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.GregorianCalendar;
+
+
 public class Utente {
+
+    public Utente(){
+        super();
+    }
+
+    //Dati Anagrafici dell'utente
+    private String cognome;
+    private String nome;
+    private GregorianCalendar dataDiNascita;
+    private String via;
+    private String numeroCivico;
+    private String CAP;
+    private String numeroTelefonico;
+
+    //Dati dell'Account
+    private String email;
+    private String password;
+
+    //Getter & Setter
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public GregorianCalendar getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(GregorianCalendar dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public String getNumeroCivico() {
+        return numeroCivico;
+    }
+
+    public void setNumeroCivico(String numeroCivico) {
+        this.numeroCivico = numeroCivico;
+    }
+
+    public String getCAP() {
+        return CAP;
+    }
+
+    public void setCAP(String CAP) {
+        this.CAP = CAP;
+    }
+
+    public String getNumeroTelefonico() {
+        return numeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        this.numeroTelefonico = numeroTelefonico;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(password.getBytes(StandardCharsets.UTF_8));
+            this.password = String.format("%040x", new BigInteger(1, digest.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
--- Code Toj/src/main/java/Model/UtenteDAO.java
+++ Code Toj/src/main/java/Model/UtenteDAO.java
@@ -1,4 +1,112 @@
 package Model;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 public class UtenteDAO {
+
+    public Utente doRetrieveByMailAccount(String email) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT * FROM account_user WHERE email=?");
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Utente d = new Utente();
+                d.setEmail(rs.getString(1));
+                d.setPassword(rs.getString(2));
+                return d;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private GregorianCalendar toCalendar(java.sql.Date date){
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTimeInMillis(date.getTime());
+        return cal;
+    }
+
+    public UtenteDAO doRetrieveByMailDatiAnagrafici(String email) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT * FROM account_user A, dati_cliente D WHERE A.Email=D.Email");
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Utente d = new Utente();
+                d.setEmail(rs.getString("Email"));
+                d.setPassword(rs.getString("Password_User"));
+                d.setNome(rs.getString("Nome"));
+                d.setCognome(rs.getString("Cognome"));
+
+                //Translazione da SQLDate A GregorianCalendar
+                java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
+                d.setDataDiNascita(toCalendar(date));
+
+                d.setNumeroTelefonico(rs.getString("Telefono"));
+                d.setVia(rs.getString("Via"));
+                d.setNumeroCivico(rs.getString("N_civico"));
+                d.setCAP(rs.getString("CAP"));
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public ArrayList<Categoria> doRetrieveAll(){
+        try (Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM categoria");
+            ResultSet rs = ps.executeQuery();
+            ArrayList<Categoria> categorie = new ArrayList<>();
+            while (rs.next()){
+                Categoria categoria = new Categoria();
+                categoria.setId(rs.getInt("id"));
+                categoria.setNome(rs.getString("nome"));
+                categoria.setDescrizione(rs.getString("descrizione"));
+                categorie.add(categoria);
+            }
+            return categorie;
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    // la funzione seguente è inutile perchè il DB è riempito tramite tool esterno
+    // sarebbe utile se l'applicazione fornisse un form per riempirlo. IDEA! aggiungi questa feature all'applicazione
+    // è un buon modo per verificare la sua correttezza
+    public void doSave(Categoria categoria) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO categoria (id, nome, descrizione) VALUES(?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, categoria.getId());
+            ps.setString(2, categoria.getNome());
+            ps.setString(3, categoria.getDescrizione());
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("INSERT error.");
+            }
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            int id = rs.getInt(1);
+            categoria.setId(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doUpdateCategoria(Categoria categoria){
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "UPDATE categoria SET nome='" + categoria.getNome() + "', descrizione='" +
+                            categoria.getDescrizione() + "' WHERE id=" + categoria.getId());
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
