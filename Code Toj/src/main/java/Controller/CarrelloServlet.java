40b580d1d5777ebbd6590e96ccb78ce4abf86f51
albmo <albmont99@gmail.com>
2021-04-14 21:48:51 +0200

    da completare

--- Code Toj/src/main/java/Controller/CarrelloServlet.java
+++ Code Toj/src/main/java/Controller/CarrelloServlet.java
@@ -1,19 +1,31 @@
 package Controller;
 
+import Model.Articolo;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "CarrelloServlet", value = "/CarrelloServlet")
 public class CarrelloServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        List<Articolo> articoli = (List<Articolo>) session.getAttribute("articoliCarrello");
+        if(articoli == null){
+            articoli = new ArrayList<>();
+        }
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
     }
 }
