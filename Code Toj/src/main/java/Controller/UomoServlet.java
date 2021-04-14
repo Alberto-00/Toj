44df53e263be328efd53aa61acb0ebca336b9ff8
albmo <albmont99@gmail.com>
2021-04-14 21:50:12 +0200

    Servlet Uomo

--- Code Toj/src/main/java/Controller/UomoServlet.java
+++ Code Toj/src/main/java/Controller/UomoServlet.java
@@ -1,19 +1,32 @@
 package Controller;
 
+import Model.Articolo;
+import Model.ArticoloDAO;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "UomoServlet", value = "/UomoServlet")
 public class UomoServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ArticoloDAO service = new ArticoloDAO();
+        ArrayList<Articolo> articoli = service.doRetrieveBySex("M");
+        request.setAttribute("articoliUomo", articoli);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/uomo.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
     }
 }
