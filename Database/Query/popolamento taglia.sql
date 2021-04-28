523e398abf6d2e1184aa623cb0f25a9179542d06
Alberto-00 <albmont99@gmail.com>
2021-04-28 22:08:47 +0200

    Query popolamento

--- Database/Query/popolamento taglia.sql
+++ Database/Query/popolamento taglia.sql
@@ -1,7 +1,7 @@
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('L');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('M');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('S');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('XL');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('XS');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('XXL');
-INSERT INTO toj_sitoweb.taglia (id_nome) VALUES ('XXXL');
+INSERT INTO taglia (id_nome) VALUES ('L');
+INSERT INTO taglia (id_nome) VALUES ('M');
+INSERT INTO taglia (id_nome) VALUES ('S');
+INSERT INTO taglia (id_nome) VALUES ('XL');
+INSERT INTO taglia (id_nome) VALUES ('XS');
+INSERT INTO taglia (id_nome) VALUES ('XXL');
+INSERT INTO taglia (id_nome) VALUES ('XXXL');
