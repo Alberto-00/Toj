65f429c00ca6d89f52d23b854d83e943ea2f8feb
Alberto-00 <albmont99@gmail.com>
2021-07-12 15:27:56 +0200

    foto

---
+++ Database/Query/popolamento admin.sql
@@ -0,0 +1,4 @@
+USE toj_sitoweb;
+INSERT INTO toj_sitoweb.Account_User (Email, Password_User, Admin_user) VALUES ('adminAlb@gmail.com', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', true);
+INSERT INTO toj_sitoweb.Account_User (Email, Password_User, Admin_user) VALUES ('adminJosef@gmail.com', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', true);
+INSERT INTO toj_sitoweb.Account_User (Email, Password_User, Admin_user) VALUES ('adminVivi@gmail.com', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', true);
